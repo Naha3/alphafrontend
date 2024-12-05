12e65708e1486d20c79e783bff90473e581c96e9
Ghulamali11 <115470547+Ghulamali11@users.noreply.github.com>
2024-12-05 12:54:02 +0500

    add component

---
+++ src/app/componets/attention/attention.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attention',
+  templateUrl: './attention.component.html',
+  styleUrl: './attention.component.scss'
+})
+export class AttentionComponent {
+
+}
