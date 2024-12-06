671b672e426ee8e4b404ded7eafd4848c8dffa06
Ghulamali11 <115470547+Ghulamali11@users.noreply.github.com>
2024-12-06 18:18:26 +0500

    6 dec

---
+++ src/app/componets/concernapproved/concernapproved.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-concernapproved',
+  templateUrl: './concernapproved.component.html',
+  styleUrl: './concernapproved.component.scss'
+})
+export class ConcernapprovedComponent {
+  status: string = 'deferred';
+
+}
