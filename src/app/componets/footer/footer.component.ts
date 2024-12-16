a3c5d7603907597cfb38bf64280788c7c0f024a8
Ghulamali11 <115470547+Ghulamali11@users.noreply.github.com>
2024-12-16 18:21:13 +0500

    footer

--- src/app/componets/footer/footer.component.ts
+++ src/app/componets/footer/footer.component.ts
@@ -6,43 +6,68 @@ import { Component, OnInit, AfterViewInit, OnDestroy, HostListener } from '@angu
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit, AfterViewInit, OnDestroy {
- 
-  showFooter: boolean = window.innerWidth > 720;
 
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.checkScreenSize();
-  }
+  isFooterVisible = false;
+  isAtBottom = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.body.scrollHeight;
+
+    // Check if the user has reached the bottom of the page
+    this.isAtBottom = scrollPosition + windowHeight >= documentHeight;
+
+    // Show the footer during scrolling but hide it at the bottom
+    this.isFooterVisible = scrollPosition > 100 && !this.isAtBottom;  }
+
+
+
+
+
+
+
+
+
+
+ 
+  // showFooter: boolean = window.innerWidth > 720;
+
+  // @HostListener('window:resize', ['$event'])
+  // onResize() {
+  //   this.checkScreenSize();
+  // }
 
   constructor() {
     this.checkScreenSize(); 
   }
 
   private checkScreenSize() {
-    this.showFooter =  window.innerWidth <= 720;;  
+    // this.showFooter =  window.innerWidth <= 720;
   }
 
   ngOnInit() {
-    this.showFooter = window.innerWidth > 720;
+    // this.showFooter = window.innerWidth > 720;
   }
   ngAfterViewInit(): void {
-    window.addEventListener('scroll', this.onScroll.bind(this));
+    // window.addEventListener('scroll', this.onScroll.bind(this));
   }
 
   ngOnDestroy(): void {
-    window.removeEventListener('scroll', this.onScroll.bind(this));
+    // window.removeEventListener('scroll', this.onScroll.bind(this));
   }
 
-  onScroll(): void {
-    const windowHeight = window.innerHeight;
-    const scrollPosition = window.scrollY;
-    const documentHeight = document.body.offsetHeight;
-    if ((windowHeight + scrollPosition) >= documentHeight) {
-      console.log('User has reached the bottom');
-      this.showFooter = true;
-    } else {
-      this.showFooter = true;
-    }
-  }
+  // onScroll(): void {
+  //   const windowHeight = window.innerHeight;
+  //   const scrollPosition = window.scrollY;
+  //   const documentHeight = document.body.offsetHeight;
+  //   if ((windowHeight + scrollPosition) >= documentHeight) {
+  //     console.log('User has reached the bottom');
+  //     // this.showFooter = true;
+  //   } else {
+  //     // this.showFooter = true;
+  //   }
+  // }
 
 }
